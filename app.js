f9aa234f766cb0a724fac07e88241cbbbd45c763
christiannnc <c917unc@gmail.com>
2023-02-15 00:31:01 -0500

    add Procfile

---
+++ app.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = 'MTA3NTI2ODUwNDUyNzMyMzE2Ng.GHgxS3.N92i1bMGn241NakM3ilvX8hrEwD5bo2UubyYaM';
+
+client.on('ready', () => {
+  console.log('Bot is now connected');
+});
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  message.channel.send(message.content);
+});
+
+client.login(token);
