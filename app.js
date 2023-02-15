6f4b858c01c4ae01a321df5dccb692563eb7f838
Christian Cox <57050814+christiannnc@users.noreply.github.com>
2023-02-15 02:17:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+const Discord require('discord.js');
+const client = new Discord.Client({
+    intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
 const token = 'MTA3NTI2ODUwNDUyNzMyMzE2Ng.Gi9vGw.2HxSZl6Fc5MQdRrxH2yaXDdrFgadiFtAs3h8b4';
