e3302c4800fc411d44ba820706a161570a042514
cmcox6 <cmcox6@ncsu.edu>
2023-02-15 02:38:46 -0500

    new commit

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-  ]
-})
-
-const token = 'MTA3NTI2ODUwNDUyNzMyMzE2Ng.Gi9vGw.2HxSZl6Fc5MQdRrxH2yaXDdrFgadiFtAs3h8b4';
-
-client.on('ready', () => {
-  console.log('Bot is now connected');
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-client.on('message', (message) => {
-  if (message.author.bot) return;
-  message.channel.send(message.content);
+const token =
+    "MTA3NTI2ODUwNDUyNzMyMzE2Ng.Gi9vGw.2HxSZl6Fc5MQdRrxH2yaXDdrFgadiFtAs3h8b4";
+
+client.on("ready", () => {
+    console.log("Bot is now connected");
+});
+
+client.on("message", (message) => {
+    if (message.author.bot) return;
+    message.channel.send(message.content);
+    console.log(message);
 });
 
 const express = require("express");
